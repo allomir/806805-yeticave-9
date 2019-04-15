860dfc2878e5107781866cc6b1395052de74c881
allomir <49421075+allomir@users.noreply.github.com>
2019-04-15 11:55:19 +0300

    Доработка modile1 task2

--- index.php
+++ index.php
@@ -27,25 +27,22 @@ $user_name = 'Михаил Лебедев'; // укажите здесь ваш�
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-         <?php 
-         if ($is_auth == 1) {
-         echo "
-  <div class=\"user-menu__logged\">
-    <p>$user_name</p>
-    <a class=\"user-menu__bets\" href=\"pages/my-bets.html\">Мои ставки</a>
-    <a class=\"user-menu__logout\" href=\"#\">Выход</a>
-  </div>
-         ";} 
-          else { echo "
-  <ul class=\"user-menu__list\">
-    <li class=\"user-menu__item\">
-      <a href=\"#\">Регистрация</a>
-    </li>
-    <li class=\"user-menu__item\">
-      <a href=\"#\">Вход</a>
-    </li>
-  </ul>
-         ";}
+            <?php if ($is_auth == 1): ?>
+            <div class=\"user-menu__logged\">
+                <p><?=$user_name; ?></p>
+                <a class=\"user-menu__bets\" href=\"pages/my-bets.html\">Мои ставки</a>
+                <a class=\"user-menu__logout\" href=\"#\">Выход</a>
+            </div>
+            <?php else: ?>
+            <ul class=\"user-menu__list\">
+                <li class=\"user-menu__item\">
+                    <a href=\"#\">Регистрация</a>
+                </li>
+                <li class=\"user-menu__item\">
+                    <a href=\"#\">Вход</a>
+                </li>
+            </ul>
+            <?php endif; ?>
          ?>
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
 

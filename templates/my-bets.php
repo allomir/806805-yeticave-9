2335ad125dc8f3de6a7f7add0363e301fd0d9e07
allomir <49421075+allomir@users.noreply.github.com>
2019-05-13 19:05:43 +0300

    Мои ставки чистка

--- templates/my-bets.php
+++ templates/my-bets.php
@@ -60,50 +60,5 @@
         </tr>
         <?php endforeach; ?>
 
-        <tr class="rates__item rates__item--win">
-          <td class="rates__info">
-            <div class="rates__img">
-              <img src="../img/rate3.jpg" width="54" height="40" alt="Крепления">
-            </div>
-            <div>
-              <h3 class="rates__title"><a href="lot.html">Крепления Union Contact Pro 2015 года размер L/XL</a></h3>
-              <p>Телефон +7 900 667-84-48, Скайп: Vlas92. Звонить с 14 до 20</p>
-            </div>
-          </td>
-          <td class="rates__category">
-            Крепления
-          </td>
-          <td class="rates__timer">
-            <div class="timer timer--win">Ставка выиграла</div>
-          </td>
-          <td class="rates__price">
-            10 999 р
-          </td>
-          <td class="rates__time">
-            Час назад
-          </td>
-        </tr>
-
-        <tr class="rates__item rates__item--end">
-          <td class="rates__info">
-            <div class="rates__img">
-              <img src="../img/rate5.jpg" width="54" height="40" alt="Куртка">
-            </div>
-            <h3 class="rates__title"><a href="lot.html">Куртка для сноуборда DC Mutiny Charocal</a></h3>
-          </td>
-          <td class="rates__category">
-            Одежда
-          </td>
-          <td class="rates__timer">
-            <div class="timer timer--end">Торги окончены</div>
-          </td>
-          <td class="rates__price">
-            10 999 р
-          </td>
-          <td class="rates__time">
-            Вчера, в 21:30
-          </td>
-        </tr>
-
       </table>
     </section>
